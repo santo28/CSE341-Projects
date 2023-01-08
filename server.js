2826a49854721ad8a233e48d7d2e9f86a945ce5c
santo28 <santos17004@byui.edu>
2023-01-08 00:05:34 -0700

    port running

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/', require('./routes'))
+app.use('/', require('./Routes'))
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`)
