b08f0e7b3ef953cec5fb1fa16d5c21447b981e23
santo28 <santos17004@byui.edu>
2023-01-16 09:34:57 -0700

    src folder added 2

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
+// const dotenv = require('dotenv')
+// dotenv.config()
+
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+
+  app.use(bodyParser.json())
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  app.use('/', require('./routes'));
+
+  mongodb.initDb((err, mongodb) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(port);
+      console.log(`Connected to DB and listening on ${port}`);
+    }
+  });
+
