4a72bac71728dbf07c06f0eaa7c3ee559c34f386
santo28 <santos17004@byui.edu>
2023-01-15 23:51:13 -0700

    Finishing Project

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use('/', require('./Routes'))
-
-app.listen(port, () => {
-    console.log(`Running on port ${port}`)
-})
